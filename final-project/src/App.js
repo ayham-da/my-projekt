9fbf46af3f62c6009e35b29304e999705768e691
ayham <ayham.alaaeddin@gmail.com>
2020-05-27 13:26:49 +0200

    import NewPlace to the App.js

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import React                                       from 'react';
 import {BrowserRouter as Router, Route , Redirect} from 'react-router-dom'
-import Users                                       from './user/pages/Users'  
+import Users                                       from './user/pages/Users' 
+import NewPlace                                    from './places/pages/NewPlace'
 
 const App = () => {
   return (
@@ -9,6 +10,10 @@ const App = () => {
     <Router>
       <Route path="/" exact>
         <Users/>
+      </Route>
+
+      <Route path="/places/new" exact>       
+        <NewPlace/>
       </Route>
       <Redirect to="/" />
     </Router>
