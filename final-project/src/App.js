f0e7642f5bc4434aad2e62865292fbe9e1ce36ff
ayham <ayham.alaaeddin@gmail.com>
2020-05-27 12:58:18 +0200

    import Users to the App.js

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,12 +1,17 @@
-import React                     from 'react';
-import {BrowserRouter as Router} from 'react-router-dom'
+import React                            from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.css';
+import Users                            from './user/pages/Users'  
 
 const App = () => {
   return (
+    <>
     <Router>
-    
+      <Route path="/"></Route>
+        <Users/>
     </Router>
+    </>
+    
   );
 }
 
