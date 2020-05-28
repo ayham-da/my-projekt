b6ad9062a396836ce99f68c5fbb85d1a4802658c
ayham <ayham.alaaeddin@gmail.com>
2020-05-28 15:32:50 +0200

    create Users array and create images file

--- final-project/src/user/pages/Users.js
+++ final-project/src/user/pages/Users.js
@@ -1,12 +1,20 @@
 import React                     from 'react';
 
 import UsersList                 from '../components/UsersList'
+import Image                     from '../../images/ayham-aboutme.jpeg'
 
 
 const Users = () => {
+    const USERS = 
+    [{
+        id: 'user1' , 
+        name: 'Ayham' , 
+        image: Image , 
+        places: 3
+    }];
     return (
         <>
-        <UsersList />
+        <UsersList items={USERS} />
         </>
     );
 };
