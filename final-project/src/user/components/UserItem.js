f8a28a44acb38e56272c112e5e8b8d51de8b10c2
ayham <ayham.alaaeddin@gmail.com>
2020-05-28 20:32:33 +0200

    write UsersList.css & UserItem.css

--- final-project/src/user/components/UserItem.js
+++ final-project/src/user/components/UserItem.js
@@ -1,15 +1,18 @@
 import React                     from 'react';
+import { Link }                  from 'react-router-dom'
+
 import Avatar                    from '../../shared/components/UIElements/Avatar'
+import './UserItem.css'
 
 const UserItem = props =>{
     return (
         <>
-        <li>
-            <div>
-                <div>
+        <li className="user-item">
+            <div className="user-item__content">
+                <div className="user-item__image">
                     <Avatar image={props.image} alt={props.name} />
                 </div>
-                <div>
+                <div className="user-item__info">
                     <h2>{props.name}</h2>
                     <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>
                 </div>
--- final-project/src/user/components/UsersList.js
+++ final-project/src/user/components/UsersList.js
@@ -1,11 +1,13 @@
 import React                     from 'react';
 import UserItem                  from './UserItem'
+import './UsersList.css'
+
 
 const UsersList = props =>{
      if (props.items.length === 0){
          return ( 
          <>
-         <div>
+         <div className="center">
              <h2>No Users found!!</h2>
          </div>
          </>
@@ -13,7 +15,7 @@ const UsersList = props =>{
      }else{
      return(
          <>
-         <ul>
+         <ul className="users-list">
              {props.items.map(user =>{
                  return(<>
                  <UserItem 
