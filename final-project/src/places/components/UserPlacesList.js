fdebaed638590aed8a051eb20d7cfa5832dc313e
ayham <ayham.alaaeddin@gmail.com>
2020-05-30 00:41:16 +0200

    change2

--- final-project/src/places/components/UserPlacesList.js
+++ final-project/src/places/components/UserPlacesList.js
@@ -4,7 +4,7 @@ import React                     from 'react';
 import './UserplacesList.css'
 
 
-const UserplacesList = () => {
+const UserplacesList = props => {
     return <h2>UserplacesList works!</h2>
 };
 export default UserplacesList;
