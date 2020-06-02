5375fbf38d8fb5bde3de2f734545ce3c9a14c5d1
ayham <ayham.alaaeddin@gmail.com>
2020-06-02 19:10:36 +0200

    import Modal from react-bootstrap

--- final-project/src/places/components/PlaceItem.js
+++ final-project/src/places/components/PlaceItem.js
@@ -1,33 +1,55 @@
-import React                     from 'react';
+import React , {useState}            from 'react';
 
-import { Link }                  from 'react-router-dom'
+import { Link }                      from 'react-router-dom'
+import Modal                         from 'react-bootstrap/Modal'
 import {Card, Button,Nav }           from 'react-bootstrap'
-import                                'bootstrap/dist/css/bootstrap.min.css';
+import                                    'bootstrap/dist/css/bootstrap.min.css';
 
-import './PlaceItem.css'
+import                                    './PlaceItem.css'
 
 
 const PlaceItem = props => {
+  const [showMap, setShowMap] = useState(false);
+
+  const openMapHandler = () => setShowMap(true);
+
+  const closeMapHandler = () => setShowMap(false);
+
     return(
-        <>
-     <li className="place-item">
-      <div className="place-item__content">
-      <div className="place-item__image">
-        <img src={props.image} alt={props.title} />
+    <React.Fragment>
+      <li className="place-item">
+        <div className="place-item__content">
+        <div className="place-item__image">
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className="place-item__info">
+          <h2>{props.title}</h2>
+          <h3>{props.address}</h3>
+          <p>{props.description}</p>
+        </div>
+        <div className="place-item__actions">
+          <Button variant="primary" onClick={openMapHandler}>VIEW ON MAP</Button>
+          <Button to={`/places/${props.id}`}>EDIT</Button>
+          <Button >DELETE</Button>
+
+        <Modal show={showMap} onHide={closeMapHandler}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={closeMapHandler}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={closeMapHandler}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        </div>
       </div>
-      <div className="place-item__info">
-        <h2>{props.title}</h2>
-        <h3>{props.address}</h3>
-        <p>{props.description}</p>
-      </div>
-      <div className="place-item__actions">
-        <Button >VIEW ON MAP</Button>
-        <Button to={`/places/${props.id}`}>EDIT</Button>
-        <Button >DELETE</Button>
-      </div>
-    </div>
-  </li>
-  </>
+    </li>
+  </React.Fragment>
   )
 };
 export default PlaceItem;
