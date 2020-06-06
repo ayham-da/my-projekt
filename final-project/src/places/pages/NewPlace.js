67df319bf8e8b23b9abd69ead1f8265ba2485f2a
ayham <ayham.alaaeddin@gmail.com>
2020-06-07 00:28:31 +0200

    write NewPlace js&css

--- final-project/src/places/pages/NewPlace.js
+++ final-project/src/places/pages/NewPlace.js
@@ -1,10 +1,40 @@
-import React                     from 'react';
+import React ,{useState}                    from 'react';
 
+import Form from 'react-bootstrap/Form'
+import Col from 'react-bootstrap/Col'
 
+import './PlaceForm.css';
 import './NewPlace.css'
 
 
 const NewPlace = () => {
-    return <h2>NewPlace works!</h2>
-};
+
+    const [enteredValue , setEnteredValue] = useState('')
+    const [isValid , setIsValid] = useState(false)
+
+
+    const changHandler = event => {}
+
+    return (
+        <div>
+            <Form className="place-form">
+                <Form.Group>
+                <Form.Row>
+                    <Form.Label column lg={2}>
+                        Normal Text
+                    </Form.Label>
+                    <Col>
+                        <Form.Control type="text" placeholder="Normal text" onChange={changHandler} />
+                    </Col>
+                    </Form.Row>
+                    <br />
+                    <Form.File
+                        id="exampleFormControlFile1"
+                        label="Example file input" 
+                    />
+                </Form.Group>
+            </Form>
+        </div>
+    
+        )};
 export default NewPlace;
