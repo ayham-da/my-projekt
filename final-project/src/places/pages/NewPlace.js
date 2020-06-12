742aae83e0350d646d923d5b253950f86f30e41a
ayham <ayham.alaaeddin@gmail.com>
2020-06-13 00:46:18 +0200

    Edit Newplace js&css /Edit Newplace js&css /Delta compression using up to 4 threads.

--- final-project/src/places/pages/NewPlace.js
+++ final-project/src/places/pages/NewPlace.js
@@ -1,37 +1,86 @@
 import React ,{useState}                    from 'react';
 
+import Input from '../../shared/components/FormElement/Input';
+import {
+    VALIDATOR_REQUIRE,
+    VALIDATOR_MINLENGTH
+  } from '../../shared/util/validators';
+  import { useForm } from '../../shared/hooks/FormHook';
+
 import Form from 'react-bootstrap/Form'
 import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+
+
 
 import './NewPlace.css'
 
 
 const NewPlace = () => {
 
-    const [enteredValue , setEnteredValue] = useState('')
-    const [isValid , setIsValid] = useState(false)
-
-
-    const changHandler = event => {}
+    const [formState, inputHandler] = useForm(
+        {
+          title: {
+            value: '',
+            isValid: false
+          },
+          description: {
+            value: '',
+            isValid: false
+          },
+          address: {
+            value: '',
+            isValid: false
+          }
+        },
+        false
+      );
+    
+      const placeSubmitHandler = event => {
+        event.preventDefault();
+        console.log(formState.inputs); // send this to the backend!
+      };
 
     return (
         <div>
-            <Form className="place-form">
-                <Form.Group>
-                <Form.Row>
-                    <Form.Label column lg={2}>
-                        Normal Text
-                    </Form.Label>
-                    <Col>
-                        <Form.Control type="text" placeholder="Normal text" onChange={changHandler} />
-                    </Col>
-                    </Form.Row>
+            <Form className="place-form" onSubmit={placeSubmitHandler}>
+                    <Input
+                        id="title"
+                        element="input"
+                        type="text"
+                        label="Title"
+                        validators={[VALIDATOR_REQUIRE()]}
+                        errorText="Please enter a valid title."
+                        onInput={inputHandler}
+                    />
+                    
+                    <Input
+                        id="description"
+                        element="textarea"
+                        label="Description"
+                        validators={[VALIDATOR_MINLENGTH(5)]}
+                        errorText="Please enter a valid description (at least 5 characters)."
+                        onInput={inputHandler}
+                    />
+                    
+                    <Input
+                        id="address"
+                        element="input"
+                        label="Address"
+                        validators={[VALIDATOR_REQUIRE()]}
+                        errorText="Please enter a valid address."
+                        onInput={inputHandler}
+                    />
                     <br />
                     <Form.File
                         id="exampleFormControlFile1"
                         label="Example file input" 
                     />
-                </Form.Group>
+                    <br />
+                    <Button type="submit" disabled={!formState.isValid}>
+                        ADD PLACE
+                    </Button>
+                
             </Form>
         </div>
     
--- final-project/src/shared/components/FormElement/Input.js
+++ final-project/src/shared/components/FormElement/Input.js
@@ -1,30 +1,92 @@
 import React, { useReducer, useEffect } from 'react';
 
+import Form from 'react-bootstrap/Form'
+import Col from 'react-bootstrap/Col'
 
-
+import { validate } from '../../util/validators';
+import './Input.css';
 
 const inputReducer = (state, action) => {
-    switch (action.type) {
-      case 'CHANGE':
-        return {
-          ...state,
-          value: action.val,
-          isValid: validate(action.val, action.validators)
-        };
-      case 'TOUCH': {
-        return {
-          ...state,
-          isTouched: true
-        }
+  switch (action.type) {
+    case 'CHANGE':
+      return {
+        ...state,
+        value: action.val,
+        isValid: validate(action.val, action.validators)
+      };
+    case 'TOUCH': {
+      return {
+        ...state,
+        isTouched: true
       }
-      default:
-        return state;
     }
+    default:
+      return state;
+  }
+};
+
+const Input = props => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: props.initialValue || '',
+    isTouched: false,
+    isValid: props.initialValid || false
+  });
+
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
+
+  useEffect(() => {
+    onInput(id, value, isValid)
+  }, [id, value, isValid, onInput]);
+
+  const changeHandler = event => {
+    dispatch({
+      type: 'CHANGE',
+      val: event.target.value,
+      validators: props.validators
+    });
   };
 
-  const Input = props => {
-    const [inputState, dispatch] = useReducer(inputReducer, {
-      value: props.initialValue || '',
-      isTouched: false,
-      isValid: props.initialValid || false
+  const touchHandler = () => {
+    dispatch({
+      type: 'TOUCH'
     });
+  };
+
+  const element =
+    props.element === 'input' ? (
+        <Form.Control
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={changeHandler}
+        onBlur={touchHandler}
+        value={inputState.value}
+      />
+    ) : (
+        <Form.Control 
+        as="textarea"
+        id={props.id}
+        rows={props.rows || 3}
+        onChange={changeHandler}
+        onBlur={touchHandler}
+        value={inputState.value}
+      />
+    );
+
+  return (
+    <Form.Group >
+      <Form.Label htmlFor={props.id} column lg={2} className={`${!inputState.isValid && inputState.isTouched &&
+          'form-control--invalid'}`}>
+            {props.label}
+      </Form.Label>
+      <div className={`${!inputState.isValid && inputState.isTouched &&
+          'form-control--invalid'}`}>
+      {element}
+      {!inputState.isValid && inputState.isTouched && <p>{props.errorText}</p>}
+      </div>
+    </Form.Group>           
+  );
+};
+
+export default Input;
