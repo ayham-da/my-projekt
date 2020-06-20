9ad996ed2991ee58f304569beafc173b65f46c70
ayham <ayham.alaaeddin@gmail.com>
2020-06-20 22:27:24 +0200

    test

--- final-project/src/Home/pages/HomePage.js
+++ final-project/src/Home/pages/HomePage.js
@@ -4,6 +4,6 @@ import React                     from 'react';
 import './HomePage.css'
 
 const HomePage = () => {
-    return <h2>HomePage works!</h2>
+    return <h2>HomePage1 works!</h2>
 };
 export default HomePage;
